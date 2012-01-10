eb57a8142bad36bf95d6df9884be5839a628b453
Pradeep Pant <pantpradeep@gmail.com>
2012-01-10 16:56:50 +0530

    Tip-s removing leading and trailing speces -- sol 1

---
+++ Tip-1.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+use strict;
+use 
+# This script will provide the solution for remving leading and trailing spaces in a string
+# Solution -1
+# These substitutions are very handly when you do comparisons. So it's always advisable to remove these extra space before you check for equality
+my $test_str = '  This is string to test leading and trailing space.  ';
+# ^ will check for spaces from starting of the string and will replace with blank
+$test_str =~ s/^\s+//; 
+# $ will check for spaces from end of the string and will replace with blank
+$test_str =~ s/\s+$//;
+# result string can be used for further opeartions
+print $test_str;
