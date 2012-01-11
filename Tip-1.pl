de0f4dacde26e1ca2f35b14145162f4d435d184e
Pradeep Pant <pantpradeep@gmail.com>
2012-01-11 15:20:32 +0530

    Adding Solution-2 for Removing leading and trailing spaces in a string

--- Tip-1.pl
+++ Tip-1.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/perl -w
 use strict;
-use 
+use warnings;
+use 5.010;
+use Data::Dumper;
+
 # This script will provide the solution for removing leading and trailing spaces in a string
 # Solution -1
 # These substitutions are very handly when you do comparisons. So it's always advisable to remove these extra space before you check for equality
@@ -10,4 +13,9 @@ $test_str =~ s/^\s+//;
 # $ will check for spaces from end of the string and will replace with blank
 $test_str =~ s/\s+$//;
 # result string can be used for further opeartions
-print $test_str;
+say $test_str;
+# Solution - 2
+# We can combine both regaular expressions in one 
+$test_str =~ s/(^\s+|\s+$)//g;
+# !Caution: Capuring might cost you some execution time .. noncapuring regx should spped up 
+say $test_str;
