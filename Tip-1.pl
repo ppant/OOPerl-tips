30dc6ecf9cd64ea60a7b8385ea7ebd05ba0a1337
Pradeep Pant <pantpradeep@gmail.com>
2013-04-02 16:03:42 +0530

    UTF-8 support

--- Tip-1.pl
+++ Tip-1.pl
@@ -1,7 +1,10 @@
-#!/usr/bin/perl -w
-use strict;
-use warnings;
-use 5.010;
+use utf8;      # so literals and identifiers can be in UTF-8
+use v5.12;     # or later to get "unicode_strings" feature
+use strict;    # quote strings, declare variables
+use warnings;  # on by default
+use warnings  qw(FATAL utf8);    # fatalize encoding glitches
+use open      qw(:std :utf8);    # undeclared streams in UTF-8
+use charnames qw(:full :short);  # unneeded in v5.16
 use Data::Dumper;
 
 # This script will provide the solution for removing leading and trailing spaces in a string
